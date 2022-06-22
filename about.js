4d9fca6efa455bd6162e9d160c930d075738c1d2
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-06-22 20:26:40 +0300

    all changes

---
+++ about.js
@@ -0,0 +1,32 @@
+//about
+var images = [
+    "heels.jpeg",
+    "orange.jpeg",
+    "glowy.jpeg"
+    
+  ]
+  var imageHead = document.getElementById("images");
+  var i = 0;
+  setInterval(function() {
+      imageHead.style.backgroundImage = "url(" + images[i] + ")";
+      i = i + 1;
+      if (i == images.length) {
+        i =  0;
+      }
+  }, 2000);
+  
+  
+  var colors = ["blue", "pink", "black"]
+  var currentColor = 0
+  var textColor = document.getElementsByTagName("h1")
+  
+  function changeColor() {
+  --currentColor
+  if (currentColor < 0) currentColor = colors.length -1
+  for (var i = 0; i < textColor.length; i++) {
+    textColor[i].style.color = colors[(currentColor +i) % colors.length]
+  }
+  }
+  
+  setInterval(changeColor, 2000)
+  
