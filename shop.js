bf1a88d53f53905c6931712184b9ead4e286a793
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-07-03 19:04:38 +0300

    added background image to two divs in two.html

--- shop.js
+++ shop.js
@@ -1,8 +1,10 @@
+//new
 var images = [
-  "images/heels.jpeg",
-  "images/orange.jpeg",
-  "images/glowy.jpeg"
-  
+    "images/myellow.jpeg",
+  "images/glowy.jpeg",
+  "images/mredjs.jpeg",
+  "images/",
+  "images/",
 ]
 var imageHead = document.getElementById("new");
 var i = 0;
@@ -13,3 +15,42 @@ setInterval(function() {
       i =  0;
     }
 }, 2000);
+
+
+//women
+var images = [
+    "images/heels.jpeg",
+    "images/orange.jpeg",
+    "images/belt.jpeg",
+    "images/ pinky.jpeg",
+    "images/girshoe.jpeg"
+  ]
+  var imageHead = document.getElementById("women");
+  var i = 0;
+  setInterval(function() {
+      imageHead.style.backgroundImage = "url(" + images[i] + ")";
+      i = i + 1;
+      if (i == images.length) {
+        i =  0;
+      }
+  }, 2000);
+
+  //men
+  var images = [
+    "images/puma.jpeg",
+    "images/",
+    "images/",
+    "images/hats.jpeg"
+    
+  ]
+  var imageHead = document.getElementById("men");
+  var i = 0;
+  setInterval(function() {
+      imageHead.style.backgroundImage = "url(" + images[i] + ")";
+      i = i + 1;
+      if (i == images.length) {
+        i =  0;
+      }
+  }, 2000);
+  
+  
